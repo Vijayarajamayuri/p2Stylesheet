bf4f032c5ed6b505a46b9448f6c4d26cb36ee194
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-02-22 12:05:08 -0600

    commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('clearall').addEventListener('click', function () {
 })
 
 console.log('get random quotes: AJAX fetch')
-
+//<button class="btn btn-outline-success my-2 my-sm-0" type="submit">convert</button>
 const url = "https://github.com/Vijayarajamayuri/p2Stylesheet/raw/master/image.jpg"
 document.getElementById('simpbutt').addEventListener('mouseover', function() {
     fetch(url) //fetch is the simplifed version of xmlhttprequest
--- test.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    QUnit.test("UnitTest", function (assert) {
-        let product = weightConverter(1);
-        assert.equal(product, "0.453592", "1 pound is equal to 0.453592 so test is passed!!")
-    })
-
-}
