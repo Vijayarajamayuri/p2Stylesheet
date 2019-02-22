b6b8492376d2e0699fbcc86e01c7d82a61141ac8
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-02-22 13:00:39 -0600

    commit

--- script.js
+++ script.js
@@ -12,14 +12,8 @@ console.log('get random quotes: AJAX fetch')
 //<button class="btn btn-outline-success my-2 my-sm-0" type="submit">convert</button>
 const url = "https://github.com/Vijayarajamayuri/p2Stylesheet/raw/master/image.jpg"
 document.getElementById('simpbutt').addEventListener('mouseover', function() {
-    fetch(url) //fetch is the simplifed version of xmlhttprequest
-    .then((res) => { return res.json() }) //receiving returning response as JSON application
-    .then((data) => {            // selecting which data retrieved from returned response to be displayed on html
-        data.forEach((item) => {
-            const { quote, character } = item
-            const result = 'Quote : "${quote}"  By : ${character}'
-            document.getElementById('simpsons').innerHTML = result;
-        });
+    //fetch(url) //fetch is the simplifed version of xmlhttprequest
+    
     })
     .catch(function (error) {
         console.log(JSON.stringify(error)); //logs in console whenever HTTP response error occurs (failed HTTP response handling)
