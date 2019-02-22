de8677050cf23ab91e176cfdedc88e0256599492
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-02-22 16:22:18 -0600

    commit

--- script.js
+++ script.js
@@ -7,16 +7,34 @@ document.getElementById('clearall').addEventListener('click', function () {
     localStorage.clear() //clear local storage
     document.getElementById('retrieving').innerHTML = ""
 })
+document.getElementById('simpbutt').addEventListener('click',function(){
+    // Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
 
-console.log('get random quotes: AJAX fetch')
-//<button class="btn btn-outline-success my-2 my-sm-0" type="submit">convert</button>
-const url = "https://github.com/Vijayarajamayuri/p2Stylesheet/raw/master/image.jpg"
-document.getElementById('simpbutt').addEventListener('mouseover', function() {
-    //fetch(url) //fetch is the simplifed version of xmlhttprequest
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
     
-    })
-    .catch(function (error) {
-        console.log(JSON.stringify(error)); //logs in console whenever HTTP response error occurs (failed HTTP response handling)
-    })
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("simpsons").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("simpsons").innerHTML = "EXPIRED";
+  }
+}, 1000);
 })
-console.log('Script End')
+ 
